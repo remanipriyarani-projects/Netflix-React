dcba7a8299ec1667d62b0b96e6b48936a7c63759
Priyarani <remanipriyarani.com>
2026-05-22 16:40:21 +0100

    netflix demo

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Banner.css'
+function Banner() {
+  return (
+    <div className='banner'>
+        <div className='content'>
+            <h1 className='title'>Movie Name</h1>
+            <div className='banner_buttons'>
+                <button className='button'>Play</button>
+                <button className='button'>My List</button>
+            </div>
+                <h1 className='description'>Lorem ipsum is a dummy or placeholder text commonly used in graphic design, publishing, and web development.</h1>
+        </div>
+    
+        <div className='fade_bottom'>
+
+        </div>
+
+      
+    </div>
+  )
+}
+
+export default Banner
